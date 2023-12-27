12eef36f38bddfece75551ff9673348878b69743
aysekardas <aysekardas7@gmail.com>
2023-12-27 22:19:15 +0300

    oop

---
+++ ConsoleUI/Student.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleUI;
+
+// Class default access modifier (erişim belirteci): internal
+class Student : User //: Object
+{
+    // default access modifier: private
+    internal string FirstName { get; set; } // Property
+    internal string LastName { get; set; } // Property
+
+    private int yas_; //Field
+
+
+    //Encapsulation
+    internal int Yas
+    {
+        get
+        {
+            return yas_;
+        }
+        set
+        {
+            if(value<0)
+            {
+                yas_ = value;
+            }
+        }
+    }
+   // Property
+
+    internal string FullName
+    {
+        get
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+    //internal string GetFullName()
+    //{
+    //    return $"{FirstName} {LastName}";
+    //}
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}";
+    }
+}
---
+++ ConsoleUI/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class User
+    {
+        internal int ID { get; set; }
+
+        internal  string FirstName { get; set; }
+        internal  string LastName { get; set; }
+        internal  string NickName { get; set; }
+        internal  string Name { get; set; }
+        internal  string Email { get; set; }
+        internal  string Password { get; set; }
+
+       
+
+    }
+}
