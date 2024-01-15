926c9745dcb52d75e76bcc7742318046a05b7934
aysekardas <aysekardas7@gmail.com>
2024-01-15 16:49:43 +0300

    EfModelDal

--- WebAPI/ServiceRegistration.cs
+++ WebAPI/ServiceRegistration.cs
@@ -5,7 +5,7 @@ using DataAccess.Concrete.InMemory;
 
 namespace WebAPI
 {
-    //Proje başlayınca bir kere referansı oluşturulacak ve properylerde bir kere oluşturulacak
+    //Proje başlayınca bir kere referansı oluşturulacak ve propertylerde bir kere oluşturulacak
     //Dışarıdan atama olmasın diye readonly
     public static class ServiceRegistration
     {
@@ -13,4 +13,6 @@ namespace WebAPI
         public static readonly IBrandService BrandService = new BrandManager(BrandDal);
 
     }
+
+    //IoC Container yapımızı kurunca Dependency Injection ile daha verimli hale gelecek
 }
