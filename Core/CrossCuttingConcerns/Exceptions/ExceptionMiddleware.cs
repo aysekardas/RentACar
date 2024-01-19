709ac765523fbcc596a4b474845c32c3a7b5ad00
aysekardas <aysekardas7@gmail.com>
2024-01-19 19:01:16 +0300

    middleware start

--- Core/CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
+++ Core/CrossCuttingConcerns/Exceptions/ExceptionMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,28 @@ using System.Threading.Tasks;
 
 namespace Core.CrossCuttingConcerns.Exceptions
 {
+    //Middleware'ler arasında bir sonraki adıma geçişi sağlar.
     public class ExceptionMiddleware
     {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            
+            //Delegate : Bir kod bütnünü temsil eder.
+            //RequestDelegate : Bir HTTP Request  akışındaki bir sonraki adımı temsil eder.
 
+            _next = next;
+
+        }
+
+        //Invoke isminde bir metod arayacak
+        //Geriye Task dönüyor async yapmamız lazım
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            //HttpContext : Bir HTTP Request akışını temsil eder.
+            //Asynchronous : Eş zamanlı olarak 
+            //Synchronous  : Sırasıyla 
+
+        }
     }
 }
