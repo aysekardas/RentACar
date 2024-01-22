c9701038fe8beb481aa5492294d50b7c80df7d00
aysekardas <aysekardas7@gmail.com>
2024-01-22 18:45:54 +0300

    update model entity

--- Entities/Concrete/Model.cs
+++ Entities/Concrete/Model.cs
@@ -15,27 +15,37 @@ namespace Entities.Concrete
             
         }
 
-        public Model(int id, string name, Brand? brand, Fuel? fuel, Transmission? transmission, Car? car)
+        public Model(int brandId, int fuelId, int transmissionId, string name, short year, decimal dailyPrice)
         {
-            Id = id;
+            BrandId = brandId;
+            FuelId = fuelId;
+            TransmissionId = transmissionId;
             Name = name;
-            Brand = brand;
-            Fuel = fuel;
-            Transmission = transmission;
-            Car = car;
+            Year = year;
+            DailyPrice = dailyPrice;
         }
 
-        public int Id { get; set; }
+        public int BrandId { get; set; } //normalizasyon
+
+        public int FuelId { get; set; }
+        public int TransmissionId { get; set; } 
 
         public string Name { get; set; }
+        public short Year { get; set; }
 
-        public Brand? Brand { get; set; } = null;
+        public decimal DailyPrice { get; set; }
 
-        public Fuel? Fuel { get; set; } = null;
+        //lazy loading
 
-        public Transmission? Transmission { get; set; } = null;
+        public Brand? Brand { get; set; } = null; //one-to-one ilişki
 
-        public Car? Car { get; set; } = null;
+        public Fuel? Fuel { get; set; } = null; //one-to-one ilişki
+
+        public Transmission? Transmission { get; set; } = null; //one-to-one ilişki
+
+        public ICollection<Car>? Cars { get; set; } = null;  //model ile car arasında one-to many ilişki var.
+
+
 
     }
 }
