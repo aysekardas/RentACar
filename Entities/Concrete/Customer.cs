68dfa3ce256714dc52769a4b99e9fb7b46d7b1ea
aysekardas <aysekardas7@gmail.com>
2024-01-28 22:18:49 +0300

    add user, customer entity

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer : Entity<int>
+    {
+        public Customer()
+        {
+        }
+
+        public int UserId { get; set; }
+
+        public User? User { get; set; } = null;
+    }
+}
---
+++ Entities/Concrete/IndividualCustomer.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class IndividualCustomer : Entity<int>
+    {
+        public IndividualCustomer()
+        {
+        }
+
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalIdentity { get; set; }
+        public User? User { get; set; } = null;
+        public Customer? Customer { get; set; } = null;
+    }
+}
