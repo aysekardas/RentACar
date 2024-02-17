1c5bda5f9a00310570f63b2dfc68c46790e25e40
aysekardas <aysekardas7@gmail.com>
2024-02-17 21:25:06 +0300

    add car requests

---
+++ Business/Requests/Car/DeleteCarRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Requests.Car
+{
+    public class DeleteCarRequest
+    {
+        public int Id { get; set; }
+
+        public DeleteCarRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Business/Requests/Car/UpdateCarRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Requests.Car
+{
+    public class UpdateCarRequest
+    {
+        public int ColorId { get; set; }
+
+        public int ModelId { get; set; }
+
+        public string CarState { get; set; }
+
+        public string Kilometer { get; set; }
+
+        public short ModelYear { get; set; }
+
+        public string Plate { get; set; }
+    }
+}
