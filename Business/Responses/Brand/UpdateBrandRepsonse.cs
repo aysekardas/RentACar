e3b25b7b845d4ec61ad1c0244c90b70e2d23e0d9
aysekardas <aysekardas7@gmail.com>
2024-02-17 21:38:47 +0300

    add brand responses

---
+++ Business/Responses/Brand/UpdateBrandRepsonse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Responses.Brand
+{
+    public class UpdateBrandRepsonse
+    {
+        public string Name { get; set; }
+    }
+}
