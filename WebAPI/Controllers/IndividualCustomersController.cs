d696295297ea4284effd4355400edc3f7cf91a03
aysekardas <aysekardas7@gmail.com>
2024-02-17 21:57:52 +0300

    add controllers

---
+++ WebAPI/Controllers/IndividualCustomersController.cs
@@ -0,0 +1,17 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndividualCustomersController : ControllerBase
+    {
+        private readonly IIndividualCustomerService _individualCustomerService;
+        public IndividualCustomersController(IIndividualCustomerService individualCustomerService)
+        {
+            _individualCustomerService = individualCustomerService;
+        }
+    }
+}
