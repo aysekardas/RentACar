d8a3489facd7cf9f5448c3c13708bdb4d960562a
aysekardas <aysekardas7@gmail.com>
2024-02-17 21:46:33 +0300

    add transmission responses

---
+++ Business/Responses/Transmission/UpdateTransmissionResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Responses.Transmission
+{
+    public class UpdateTransmissionResponse
+    {
+        public string Name { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
